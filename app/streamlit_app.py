fd149f98326b1c38aef1b2ecf361e7679c84d5e1
ccleb152 <caroline.leblanc152@gmail.com>
2026-09-23 11:34:02 -0500

    Update streamlit_app.py

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -48,5 +48,5 @@ home_page = st.Page(
     default=True,
 )
 
-navigation = st.navigation([tire_fitting_page, fit_diagnostics_page])
+navigation = st.navigation([home_page, tire_fitting_page, fit_diagnostics_page])
 navigation.run()
